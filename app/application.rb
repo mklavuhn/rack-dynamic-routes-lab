22629da4094af13d0c92915e34a98f66ac68cdc0
mklavuhn <michaelklavuhn3@gmail.com>
2022-01-12 16:58:41 -0800

    commit tree 68954d941a2c8c83f94fcf47a912c7357b0960e2

---
+++ app/application.rb
@@ -0,0 +1,28 @@
+class Application
+
+    @@items = []
+
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+
+        if req.path.match(/items/)
+            item_name = req.path.split("/items/").last 
+            found_item = @@items.find do |item|
+                item.name == item_name 
+            end
+            if found_item
+                resp.write found_item.price
+            else
+                resp.write "Item not found"
+                resp.status = 400
+            end
+
+        else
+            resp.write "Route not found"
+            resp.status = 404
+        end
+
+        resp.finish
+    end
+end
